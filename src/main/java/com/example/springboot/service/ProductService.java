4dd63b1ab596a909f27b314e1168345f92284fed
Vietcuong0508 <cuongtvth2009009@fpt.edu.vn>
2022-06-20 22:42:25 +0700

    update

---
+++ src/main/java/com/example/springboot/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.example.springboot.service;
+
+import com.example.springboot.entity.Product;
+import com.example.springboot.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(int id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product account) {
+        return productRepository.save(account);
+    }
+
+    public void deleteById(int id) {
+        productRepository.deleteById(id);
+    }
+}
--- src/main/java/com/example/springboot/util/StringUtil.java
+++ src/main/java/com/example/springboot/util/StringUtil.java
@@ -8,7 +8,7 @@ public class StringUtil {
     private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
     private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
 
-    private static String toSlug(String input) {
+    public static String toSlug(String input) {
         String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
         String normalized = Normalizer.normalize(nowhitespace, Normalizer.Form.NFD);
         String slug = NONLATIN.matcher(normalized).replaceAll("");
