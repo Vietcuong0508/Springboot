a40a7ad8477122db95fea33e0a56bfbaff4b37ff
Vietcuong0508 <cuongtvth2009009@fpt.edu.vn>
2022-06-21 08:47:52 +0700

    update

--- src/main/java/com/example/springboot/service/ProductService.java
+++ src/main/java/com/example/springboot/service/ProductService.java
@@ -3,10 +3,11 @@ package com.example.springboot.service;
 import com.example.springboot.entity.Product;
 import com.example.springboot.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
